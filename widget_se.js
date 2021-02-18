ac8de4eb53ce9e41a67887525b6516b02803067e
SeraphinBoccacci <47139070+SeraphinBoccacci@users.noreply.github.com>
2021-02-18 14:31:03 +0100

    Update widget_se.js

--- widget_se.js
+++ widget_se.js
@@ -1,27 +1,38 @@
+var containerIndex = 0;
+
 const appendAnimation = (herotag, amount, message) => {
+  containerIndex++;
+
   $(`
-      <div id="widget-container" class="container">
-	<audio autoplay src="https://streamparticles.io/audios/audio_1.mp3"></audio>
-        <img class="logo"
-          src="https://streamparticles.io/images/image_1.gif"
-        />
+      <div class="widget-container" id=${`widget-container-${containerIndex}`} class="container">
         <h3>${herotag} sent ${amount} ¤eGLD¤</h3>
         <div class="p-container">
           <p>${message}</p>
         </div>
       </div>
   `).appendTo(document.body);
-  setTimeout(() => {
-    $("#widget-container").addClass("slide-out");
+  
     setTimeout(() => {
-      $("#widget-container").remove();
+    $(`#widget-container-${containerIndex}`).prepend(`
+      <audio autoplay src="https://streamparticles.io/audios/audio_2.mp3"></audio>
+            <img class="logo"
+              src="https://streamparticles.io/images/image_4.gif"
+            />
+    `);
+  }, 700);
+
+  setTimeout(() => {
+    $(`#widget-container-${containerIndex}`).addClass("slide-out");
+
+    setTimeout(() => {
+      $(`#widget-container-${containerIndex}`).remove();
     }, 1000);
-  }, 10000);
+  }, 10700);
 };
 
 const socket = io("https://streamparticles.io", {
   query: {
-    streamerHerotag: 'serabocca06.elrond',
+    streamerHerotag: "serabocca06.elrond",
   },
 });
 
