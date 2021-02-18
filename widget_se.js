1e383f6f32b2fc502a39d4713996210e3921250f
SeraphinBoccacci <47139070+SeraphinBoccacci@users.noreply.github.com>
2021-02-18 14:46:02 +0100

    Update widget_se.js

--- widget_se.js
+++ widget_se.js
@@ -2,18 +2,20 @@ var containerIndex = 0;
 
 const appendAnimation = (herotag, amount, message) => {
   containerIndex++;
+  
+  const currentIndex = containerIndex
 
   $(`
-      <div class="widget-container" id=${`widget-container-${containerIndex}`} class="container">
+      <div class="widget-container" id=${`widget-container-${currentIndex}`} class="container">
         <h3>${herotag} sent ${amount} ¤eGLD¤</h3>
         <div class="p-container">
-          <p>${message}</p>
+          <p>${message} ${currentIndex}</p>
         </div>
       </div>
   `).appendTo(document.body);
   
     setTimeout(() => {
-    $(`#widget-container-${containerIndex}`).prepend(`
+    $(`#widget-container-${currentIndex}`).prepend(`
       <audio autoplay src="https://streamparticles.io/audios/audio_2.mp3"></audio>
             <img class="logo"
               src="https://streamparticles.io/images/image_4.gif"
@@ -22,10 +24,10 @@ const appendAnimation = (herotag, amount, message) => {
   }, 700);
 
   setTimeout(() => {
-    $(`#widget-container-${containerIndex}`).addClass("slide-out");
+    $(`#widget-container-${currentIndex}`).addClass("slide-out");
 
     setTimeout(() => {
-      $(`#widget-container-${containerIndex}`).remove();
+      $(`#widget-container-${currentIndex}`).remove();
     }, 1000);
   }, 10700);
 };
