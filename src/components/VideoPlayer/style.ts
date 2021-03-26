d4ffb08299c6e41cf40b2d09f8600ac8246a9b10
SeraphinBoccacci <seraphinboccacci@gmail.com>
2021-03-26 16:12:18 +0100

    make video player component responsive

---
+++ src/components/VideoPlayer/style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const VideoPlayerContainer = styled.div`
+  width: 90%;
+  max-width: 40rem;
+`;
+
+export const VideoPlayerContent = styled.div`
+  position: relative;
+  padding-top: 56.25%;
+`;
