839478c14f9456bbbe6d3c75702faec18d2bff36
SeraphinBoccacci <seraphinboccacci@gmail.com>
2021-03-29 17:59:41 +0200

    add google analytics

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,6 +2,7 @@ interface Config {
   url: string;
   apiUrl: string;
   uploadsUrl: string;
+  googleAnalyticsStreamId?: string;
 }
 
 type EnvConfig = { [key: string]: Config };
@@ -16,11 +17,13 @@ const config: EnvConfig = {
     url: "https://staging.streamparticles.io",
     apiUrl: "https://staging.streamparticles.io/api",
     uploadsUrl: "https://staging.streamparticles.io/uploads",
+    googleAnalyticsStreamId: "G-SC8RWK3MWS",
   },
   production: {
     url: "https://streamparticles.io",
     apiUrl: "https://streamparticles.io/api",
     uploadsUrl: "https://streamparticles.io/uploads",
+    googleAnalyticsStreamId: "G-EG1CB2TKV8",
   },
 };
 
