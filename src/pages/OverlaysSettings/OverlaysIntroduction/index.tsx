9f0396c226cb2de3b7f429a2a50d9f94a847f88f
SeraphinBoccacci <seraphinboccacci@gmail.com>
2021-06-09 12:21:48 +0200

    updte overlay settings introduction

--- src/pages/OverlaysSettings/OverlaysIntroduction/index.tsx
+++ src/pages/OverlaysSettings/OverlaysIntroduction/index.tsx
@@ -6,23 +6,13 @@ const OverlaysIntroduction = () => {
   return (
     <ContentContainer elevation={3} variant="elevation">
       <Paragraph>
-        <Emphasize>
-          <a target="about:blank" href="https://streamelements.com/">
-            Stream Elements
-          </a>
-        </Emphasize>
-        is an all-in-one platform that helps creators along all their content
-        management.
-      </Paragraph>
-      <Paragraph>
-        For instance, it lets you create customized overlays to react to
-        viewers&rsquo; support. That&rsquo;s what we&rsquo;ll be working on in
-        this particle.
+        <Emphasize>StreamParticles</Emphasize>
+        allows you to create your own alerts and other on stream features.
       </Paragraph>
       <Paragraph>
         You can either choose a base template which is the basic StreamParticles
         animation and sound or go with your own GIFs and sounds that will be
-        triggered depending on the variations you set.
+        triggered depending on the variations you set
       </Paragraph>
     </ContentContainer>
   );
