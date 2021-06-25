28938060a5f72ac88b9e1e94c832ed2fa7831ca2
SeraphinBoccacci <seraphinboccacci@gmail.com>
2021-06-26 00:43:44 +0200

    unblur cursor with scaling + remove scroll bar on overlay

--- src/components/overlays/widgets/donationBars/styles.lineDonationBar.ts
+++ src/components/overlays/widgets/donationBars/styles.lineDonationBar.ts
@@ -92,43 +92,52 @@ interface CursorContainerProps {
   duration?: number;
 }
 
-const computeCursorMinorOffset = (size: number) => {
-  return `-${size * 0.1}px`;
+const computeCursorMinorOffset = (size: number, scale: number) => {
+  return `${(size - size * scale * 1.2) / 2}px`;
 };
 
-const computeCursorMajorOffset = (size: number) => {
-  return `${size * 0.6}px`;
+const computeCursorMajorOffset = (size: number, scale: number) => {
+  return `${(size * scale * 1.2) / 2}px`;
 };
 
-const computeCursorSize = (size: number) => {
-  return `${size * 1.2}px`;
+const computeCursorSize = (size: number, scale: number) => {
+  return `${size * scale * 1.2}px`;
 };
 
 export const resolveCursorPosition = (display: DonationBarDisplays) => {
   if (display === DonationBarDisplays.Vertical)
     return css<CursorContainerProps>`
-      bottom: ${({ progression, containerWidth = 50 }) =>
-        `calc(${progression}% - ${computeCursorMajorOffset(containerWidth)})`};
-      left: ${({ containerWidth = 50 }) =>
-        computeCursorMinorOffset(containerWidth)};
-      width: ${({ containerWidth = 50 }) => computeCursorSize(containerWidth)};
-      height: ${({ containerWidth = 50 }) => computeCursorSize(containerWidth)};
+      bottom: ${({ progression = 0, containerWidth = 50, scale = 1 }) =>
+        `calc(${progression}% - ${computeCursorMajorOffset(
+          containerWidth,
+          scale
+        )})`};
+      left: ${({ containerWidth = 50, scale = 1 }) =>
+        computeCursorMinorOffset(containerWidth, scale)};
+      width: ${({ containerWidth = 50, scale = 1 }) =>
+        computeCursorSize(containerWidth, scale)};
+      height: ${({ containerWidth = 50, scale = 1 }) =>
+        computeCursorSize(containerWidth, scale)};
     `;
 
   return css<CursorContainerProps>`
-    bottom: ${({ containerHeight = 50 }) =>
-      computeCursorMinorOffset(containerHeight)};
-    left: ${({ progression, containerHeight = 50 }) =>
-      `calc(${progression}% - ${computeCursorMajorOffset(containerHeight)})`};
-    width: ${({ containerHeight = 50 }) => computeCursorSize(containerHeight)};
-    height: ${({ containerHeight = 50 }) => computeCursorSize(containerHeight)};
+    bottom: ${({ containerHeight = 50, scale = 1 }) =>
+      computeCursorMinorOffset(containerHeight, scale)};
+    left: ${({ progression = 0, containerHeight = 50, scale = 1 }) =>
+      `calc(${progression}% - ${computeCursorMajorOffset(
+        containerHeight,
+        scale
+      )})`};
+    width: ${({ containerHeight = 50, scale = 1 }) =>
+      computeCursorSize(containerHeight, scale)};
+    height: ${({ containerHeight = 50, scale = 1 }) =>
+      computeCursorSize(containerHeight, scale)};
   `;
 };
 
 export const CursorContainer = styled.div<CursorContainerProps>`
   position: absolute;
   z-index: 100;
-  transform: ${({ scale = 1 }) => `scale(${scale})`};
   ${({ display = DonationBarDisplays.Horizontal }) =>
     resolveCursorPosition(display)}
   transition: ${({ duration = 1 }) => `${duration}s`};
